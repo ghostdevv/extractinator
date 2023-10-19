709477df319a99a08396c667457c19478190de09
GHOST <ghostdevbusiness@gmail.com>
2023-10-19 00:34:58 +0000

    fix: impliment workaround

--- src/emit.ts
+++ src/emit.ts
@@ -1,23 +1,28 @@
+import { relative, resolve } from 'node:path'
 import { rm, mkdir } from 'node:fs/promises'
 import { createRequire } from 'node:module'
 import { emitDts } from 'svelte2tsx'
-import { resolve } from 'node:path'
 
 const require = createRequire(import.meta.url)
 
-const TEMP_DIR = '.extractinator-temp'
+const TEMP_DIR = resolve('.extractinator/dts')
 
 export async function emit(input: string) {
+	//? Cleanup & Create the TEMP_DIR
 	await rm(TEMP_DIR, { force: true, recursive: true })
 	await mkdir(TEMP_DIR, { recursive: true })
 
+	//? Use svelte2tsx to generate the dts files for Svelte/TS/JS
 	await emitDts({
+		//? Path to Shims
 		svelteShimsPath: require.resolve('svelte2tsx/svelte-shims-v4.d.ts'),
-		declarationDir: TEMP_DIR,
+		//? Relative path for the output, abs path won't work here
+		declarationDir: relative(process.cwd(), TEMP_DIR),
+		//? Input
 		libRoot: input,
 	})
 
 	return {
-		location: resolve(TEMP_DIR),
+		location: TEMP_DIR,
 	}
 }

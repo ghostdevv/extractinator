15e2681303b5b76f5b55c53ce0421f13b162f777
GHOST <ghostdevbusiness@gmail.com>
2023-11-05 15:37:54 +0000

    feat: export cli and package

---
+++ src/exports/package.ts
@@ -0,0 +1,12 @@
+export { extractinator } from '../extractinator'
+
+export type {
+	BaseParsedFile,
+	ParsedFile,
+	ParsedSvelteFile,
+	ParsedTSFile,
+	TSDocComment,
+	ExportBit,
+	SlotBit,
+	Bit,
+} from '../types.d'
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-	splitting: false,
-	sourcemap: false,
-	clean: true,
-	dts: true,
-	keepNames: true,
-	target: 'esnext',
-	format: ['esm'],
-	entryPoints: ['src/cli.ts'],
-})
+export default defineConfig([
+	{
+		splitting: false,
+		sourcemap: false,
+		clean: true,
+		dts: true,
+		keepNames: true,
+		target: 'esnext',
+		format: ['esm', 'cjs'],
+		entryPoints: ['src/exports/package.ts'],
+		shims: true,
+	},
+	{
+		splitting: false,
+		sourcemap: false,
+		clean: true,
+		dts: false,
+		keepNames: true,
+		target: 'esnext',
+		format: ['esm'],
+		entryPoints: ['src/exports/cli.ts'],
+	},
+])
